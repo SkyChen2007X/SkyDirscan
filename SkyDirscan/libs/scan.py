58f6ef1ac5420f820b3a8c2b2eb5654772d40bcb
SkyChen2007X <2656494160@qq.com>
2022-09-01 21:46:43 +0800

    Share my dirscan tool.

---
+++ SkyDirscan/libs/scan.py
@@ -0,0 +1,171 @@
+import requests
+import os
+from random import choice
+from concurrent.futures import ThreadPoolExecutor as Pool
+from concurrent.futures import wait
+from threading import Lock
+from sys import exit
+from colorama import init, Fore
+
+class Scan:
+    def __init__(self, help):
+        init(autoreset = True)
+        self.help = help
+        self.lock = Lock()
+        self.code = {
+            '200' : Fore.GREEN,
+            '403' : Fore.RED,
+            '429' : Fore.YELLOW,
+            '500' : Fore.RED
+            }
+
+        with open('D:/Tools/Web/DirScanner/skyscan/Windows/2.2/file/User-Agent.txt') as headers:
+            self.headers = headers.read().split('\n')
+            if '' in self.headers:
+                self.headers.remove('')
+
+    def test_proxy(self, proxies):
+        try:
+            requests.get('https://www.baidu.com', proxies = proxies, timeout = 5)
+            return True
+        except:
+            return False
+
+    def check_options(self, url, thread, file, timeout, proxy, status):
+        #check url
+        if url == None:
+            print(Fore.RED + '[-]ERROR: URL can\'t be none.')
+            print(Fore.YELLOW + self.help)
+            exit()
+        else:
+            self.url = url.strip('/')
+        
+        #check thread
+        
+        if thread == None:
+            self.thread = 600
+        elif thread > 1000:
+            print(Fore.RED + '[-]ERROR: Too many threads.')
+            self.thread = 600
+        elif thread <= 0:
+            print(Fore.RED + '[-]ERROR: Threads is below 0.')
+            self.thread = 600
+        else:
+            self.thread = thread
+
+        #check dictionary file
+
+        if file == None:
+            self.file = 'D:/Tools/Web/DirScanner/skyscan/Windows/2.2/file/dicc.txt'
+        else:
+            try:
+                f = open(file)
+                f.close()
+                self.file = file
+            except FileNotFoundError:
+                print(Fore.RED + '[-]ERROR: Can\'t find the file.')
+                print(Fore.YELLOW + '[?]Continue with local file?(y/n)')
+                if input() == 'y':
+                    self.file = 'D:/Tools/Web/DirScanner/skyscan/Windows/2.2/file/dicc.txt'
+                else:
+                    exit()
+
+        #check timeout
+
+        if timeout == None:
+            self.timeout = 2
+        elif timeout <= 0:
+            print(Fore.RED + '[-]ERROR:Timeout is below 0.')
+            self.timeout = 2
+        else:
+            self.timeout = timeout
+
+        #check proxy
+
+        if proxy == None:
+            self.proxies = [None]
+        else:
+            self.proxies = proxy.split(',')
+            if '' in self.proxies:
+                self.proxies.remove('')
+
+            for p in self.proxies:
+                proxies = {
+                    'http' : 'http://' + p,
+                    'https' : 'https://' + p
+                    }
+                if self.test_proxy(proxies) == False:
+                    self.proxies.remove(p)
+
+        if len(self.proxies) == 0:
+            print(Fore.RED + '[-]ERROR: The proxies is not useful.')
+            self.proxies = [None]
+
+        #check status code
+
+        if status == None:
+            self.status = None
+        else:
+            self.status = status.split(',')
+
+            for s in self.status:
+                if s  not in self.code:
+                    self.code[s] = 'green'
+
+
+    def get(self, url):
+        headers = {
+            'User-Agent' : choice(self.headers)
+            }
+        
+        try:
+            if choice(self.proxies) != None:
+                proxies = {
+                    'http' : 'http://' + choice(self.proxies),
+                    'https' : 'https://' + choice(self.proxies)
+                    }
+            else:
+                proxies = None
+            
+            s = requests.get(url, timeout = self.timeout, proxies = proxies, headers = headers)
+            self.lock.acquire()
+            try:
+                if self.status == None:
+                    if s.status_code != 404:
+                        if str(s.status_code) in self.code:
+                            print(self.code[str(s.status_code)] + '[*]' + str(s.status_code) + ' ' + url)
+                        else:
+                            print(Fore.RED + '[*]' + str(s.status_code) + ' ' + url)
+                else:
+                    if str(s.status_code) in self.status:
+                        print(self.code[str(s.status_code)] + '[*]' + str(s.status_code) + ' ' + url)
+                self.lock.release()
+                return 0
+            except KeyboardInterrupt:
+                self.lock.release()
+                exit()
+        except KeyboardInterrupt:
+            self.lock.release()
+            exit()
+        except:
+            return 1
+
+
+
+    def scan(self):
+        print(Fore.BLUE + '--<start>--')
+
+        with open(self.file) as urls:
+            e = Pool(max_workers = self.thread)
+            t = [e.submit(self.get, (self.url + '/' + d.strip('\n'))) for d in urls]
+
+            if wait(t, return_when = 'ALL_COMPLETED'):
+                print(Fore.BLUE + '--<end>--')
+                exit()
+
+
+
+        
+
+        
+        
---
+++ SkyDirscan/skydirscan.py
@@ -0,0 +1,89 @@
+help = '''
+Author: Sky_Chen
+
+Name: SkyDirScan
+
+usage: skydirscan.py [-h] [-url URL] [-timeout TIMEOUT] [-thread THREAD]
+                     [-status STATUS] [-file FILE] [-proxy PROXY]
+
+A tool to scan the dir from the target
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -url URL, -u URL      The url to scan
+  -timeout TIMEOUT      Set the timeout
+  -thread THREAD        Set the thread(max 1000)
+  -status STATUS, -s STATUS
+                        Set the right status
+  -file FILE, -f FILE   An option for your dictionary
+  -proxy PROXY, -p PROXY
+                        Set the proxies
+'''
+
+import argparse
+from sys import exit
+from colorama import init, Fore
+from libs.scan import Scan
+
+def main():
+    init(autoreset = True)
+    
+    icon = '''
+
+ (           (                              
+ )\ )   )    )\ )                           
+(()/(( /((  (()/( (  (             )        
+ /(_))\())\ )/(_)))\ )(  (   (  ( /(  (     
+(_))((_)(()/(_))_((_|()\ )\  )\ )(_)) )\ )  
+/ __| |(_)(_))   \(_)((_|(_)((_|(_)_ _(_/(  
+\__ \ / / || | |) | | '_(_-< _|/ _` | ' \)) 
+|___/_\_\\_,  |___/|_|_| /__|__|\__,_|_||_|  
+         |__/                               
+                                                                               
+
+Author: Sky Chen
+Name: SkyDirscan
+        '''
+
+
+    print(Fore.GREEN + icon)
+    
+    
+    p = argparse.ArgumentParser(description = 'A tool to scan the dir from the target')
+    p.add_argument('-url','-u', type = str, help = 'The url to scan')
+    p.add_argument('-timeout', type = int, help = 'Set the timeout')
+    p.add_argument('-thread', type = int, help = 'Set the thread(max 1000)')
+    p.add_argument('-status', '-s', type = int, help = 'Set the right status')
+    p.add_argument('-file', '-f', type = str, help = 'An option for your dictionary')
+    p.add_argument('-proxy', '-p', type = str, help = 'Set the proxies')
+    args = p.parse_args()
+    
+    url = args.url
+    timeout = args.timeout
+    thread = args.thread
+    status = args.status
+    file = args.file
+    proxy = args.proxy
+
+    scan = Scan(help)
+    scan.check_options(url, thread, file, timeout, proxy, status)
+    scan.scan()
+
+if __name__ == '__main__':
+    main()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
